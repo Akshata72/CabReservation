79f2b65c724f61409b07960e66c48718357daa7c
Akshata72 <sableakshata880@gmail.com>
2022-04-15 05:37:13 +0530

    Added:UC2-Multiple_Ride

--- Cabreservation/Program.cs
+++ Cabreservation/Program.cs
@@ -10,11 +10,24 @@ namespace Cabreservation
         readonly int PricePerKm;
         readonly int PricePerMin;
         readonly int MinimumFare;
+        public int numOfRides;
+        public double totalFare;
+        public double averagePerRide;
         public InvoiceGenerator()
         {
             this.PricePerKm = 10;
             this.PricePerMin = 1;
             this.MinimumFare = 5;
+        }
+        public double TotalFareForMultipleRides(List<Ride> rides)
+        {
+            foreach (Ride ride in rides)
+            {
+                totalFare += TotalFareForSingleRide(ride);
+                numOfRides += 1;
+            }
+            averagePerRide = totalFare / numOfRides;
+            return totalFare;
         }
         public double TotalFareForSingleRide(Ride ride)
         {
--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 
 namespace Cabreservation
 {
@@ -17,7 +18,7 @@ namespace Cabreservation
         /// <param name="distance"></param>
         /// <param name="time"></param>
         [Test]
-        [TestCase(5,3)]
+        [TestCase(5, 3)]
         public void GivenTimeAndDistance_CalculateFare(double distance, double time)
         {
             Ride ride = new Ride(distance, time);
@@ -43,6 +44,23 @@ namespace Cabreservation
             Ride ride = new Ride(1, -1);
             CabException cabException = Assert.Throws<CabException>(() => invoiceGenerator.TotalFareForSingleRide(ride));
             Assert.AreEqual(cabException.type, CabException.ExceptionType.INVALID_TIME);
+        }
+        /// <summary>
+        /// Total Fare For Multiple Rides..
+        /// </summary>
+        [Test]
+        public void GivenListOfRides_GenerateInvoice()
+        {
+            Ride ride1 = new Ride(3, 4);
+            Ride ride2 = new Ride(2, 2);
+
+            List<Ride> rides = new List<Ride>();
+            rides.Add(ride1);
+            rides.Add(ride2);
+
+            Assert.AreEqual(56, invoiceGenerator.TotalFareForMultipleRides(rides));
+            Assert.AreEqual(28, invoiceGenerator.averagePerRide);
+            Assert.AreEqual(2, invoiceGenerator.numOfRides);
         }
     }
 }
